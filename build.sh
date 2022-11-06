8770cfbdb90bf9f6b110ff22f7abce5544eb2b5b
Michael Scherer <misc@redhat.com>
2022-11-06 15:55:18 +0100

    COmpress the file from site

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #/bin/bash
+for i in $(find site/ -name *html) ; do echo $i; gzip $i ; done
+
+[ -f site/atom.xml ] && gzip site/atom.xml
+
 RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target x86_64-unknown-linux-gnu
