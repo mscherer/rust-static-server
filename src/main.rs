6808ff74765ac3e21fe2ab28c531c71ed5666b65
Michael Scherer <misc@redhat.com>
2022-11-06 15:26:34 +0100

    Add support for html file stored as .gz

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 extern crate hyper;
 extern crate include_dir;
 
+use debug_print::debug_println;
+use flate2::read::GzDecoder;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use include_dir::{include_dir, Dir};
 use std::convert::Infallible;
-use std::net::SocketAddr;
 use std::env;
+use std::io::Read;
+use std::net::SocketAddr;
 
 static WEBSITE_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/site");
 
@@ -19,44 +22,54 @@ async fn get_file(req: Request<Body>) -> Result<Response<Body>, Infallible> {
             if requested_path.ends_with('/') {
                 requested_path.push_str("index.html");
             };
+            requested_path.push_str(".gz");
 
             // Remove / at the start
             let path = &requested_path[1..];
+            debug_println!("Looking for {}", path);
 
             match WEBSITE_DIR.get_file(path) {
                 Some(path) => {
-                    let body = path.contents_utf8().unwrap();
+                    let mut gz = GzDecoder::new(path.contents());
+                    let mut body = String::new();
+                    gz.read_to_string(&mut body).unwrap();
+
                     Ok(Response::new(Body::from(body)))
                 }
                 None => {
-                    let mut res = Response::new(Body::from("not found"));
-                    *res.status_mut() = StatusCode::NOT_FOUND;
-                    Ok(res)
+                    // remove .gz
+                    let path = &requested_path[1..requested_path.len() - 3];
+                    debug_println!("Looking for {}", path);
+                    match WEBSITE_DIR.get_file(path) {
+                        Some(path) => {
+                            let body = path.contents_utf8().unwrap();
+                            Ok(Response::new(Body::from(body)))
+                        }
+                        None => Ok(Response::builder()
+                            .status(StatusCode::NOT_FOUND)
+                            .body(Body::from("not found"))
+                            .unwrap()),
+                    }
                 }
             }
         }
-        _ => {
-            let mut res = Response::new(Body::from("not implemented"));
-            *res.status_mut() = StatusCode::NOT_IMPLEMENTED;
-            Ok(res)
-        }
+        _ => Ok(Response::builder()
+            .status(StatusCode::NOT_IMPLEMENTED)
+            .body(Body::from("not implemented"))
+            .unwrap()),
     }
 }
 
 #[tokio::main]
 async fn main() {
     let port: u16 = match env::var("PORT") {
-        Ok(val) => {
-            val.parse().unwrap()
-        }
+        Ok(val) => val.parse().unwrap(),
         Err(_) => 3000,
     };
 
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
 
-    let make_svc = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(get_file))
-    });
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(get_file)) });
 
     let server = Server::bind(&addr).serve(make_svc);
 
