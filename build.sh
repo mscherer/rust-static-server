791dddbe305fc2eb0635720a806e9eea94cb882f
Michael Scherer <misc@redhat.com>
2022-11-06 19:25:16 +0100

    Strip the binary, massively shave the size

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ for i in $(find site/ -name *html) ; do echo $i; gzip $i ; done
 [ -f site/atom.xml ] && gzip site/atom.xml
 
 RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target x86_64-unknown-linux-gnu
+strip target/x86_64-unknown-linux-gnu/release/static_http
