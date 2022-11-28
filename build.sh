5d3a93303bee9a61187adf4733a7c7e565ad3d4c
Michael Scherer <misc@redhat.com>
2022-11-29 00:46:31 +0100

    Refactor the target, in case I am targetting arm

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #/bin/bash
+
+TARGET=x86_64-unknown-linux-gnu
+
 for i in $(find site/ -name *html -o -name atom.xml -o -name rss.xml) ; do echo $i; gzip $i ; done
 
-RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target x86_64-unknown-linux-gnu
-strip target/x86_64-unknown-linux-gnu/release/static_http
+RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target $TARGET
+strip target/$TARGET/release/static_http
