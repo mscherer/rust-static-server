9e6b95baf9511b7dca343c909eb6a44ea57b3b05
Michael Scherer <misc@redhat.com>
2022-11-29 00:45:39 +0100

    Compress rss.xml and atom.xml at once

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #/bin/bash
-for i in $(find site/ -name *html) ; do echo $i; gzip $i ; done
-
-[ -f site/atom.xml ] && gzip site/atom.xml
+for i in $(find site/ -name *html -o -name atom.xml -o -name rss.xml) ; do echo $i; gzip $i ; done
 
 RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target x86_64-unknown-linux-gnu
 strip target/x86_64-unknown-linux-gnu/release/static_http
