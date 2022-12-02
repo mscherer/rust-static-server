0cadf8e36b7aae79148cfba388a80053592a540b
Michael Scherer <misc@redhat.com>
2022-12-02 02:35:58 +0100

    Fix the location of the website

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 TARGET=x86_64-unknown-linux-gnu
 
-for i in $(find site/ -name *html -o -name atom.xml -o -name rss.xml) ; do echo $i; gzip $i ; done
+for i in $(find public/ -name *html -o -name atom.xml -o -name rss.xml) ; do echo $i; gzip $i ; done
 
 RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target $TARGET
 strip target/$TARGET/release/static_http
