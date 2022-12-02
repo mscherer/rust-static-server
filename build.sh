a3cbf8c7dbab95c069729f3b99fe5bf1c12f78bf
Michael Scherer <misc@redhat.com>
2022-12-02 17:24:25 +0100

    Let cargo do the stripping

--- build.sh
+++ build.sh
@@ -5,4 +5,3 @@ TARGET=x86_64-unknown-linux-gnu
 for i in $(find public/ -name *html -o -name atom.xml -o -name rss.xml) ; do echo $i; gzip $i ; done
 
 RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target $TARGET
-strip target/$TARGET/release/static_http
