9cdd308d4ecbea186e35c42306661142dd66c53b
Michael Scherer <misc@redhat.com>
2022-12-02 02:37:46 +0100

    Listen on every address

--- src/main.rs
+++ src/main.rs
@@ -75,7 +75,7 @@ async fn main() {
         Err(_) => 8088,
     };
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
 
     let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(get_file)) });
 
